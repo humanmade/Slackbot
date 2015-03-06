e0071f3f7aa8ec61868738a5231dfbe18754bd5e
Ryan McCue <me@ryanmccue.info>
2015-03-06 15:29:30 +1000

    Add join/reload commands

--- plugin.php
+++ plugin.php
@@ -39,7 +39,58 @@ add_action( 'hm.slack.bot.message.message', function ( $message, $bot ) {
 	}
 
 	do_action( 'hm.slack.bot.message', $message, $bot );
-} );
+}, 10, 2 );
+
+/**
+ * Join channels on request
+ */
+add_action( 'hm.slack.bot.message', function ( $message, $bot ) {
+	$pattern = '/^' . $bot->get_matchable_name() . ':? join <#(\w+)>/i';
+	if ( ! preg_match( $pattern, $message->text, $matches ) ) {
+		return;
+	}
+	if ( ! $bot->is_admin( $message->user ) ) {
+		$bot->send( array(
+			'type' => 'message',
+			'channel' => $message->channel,
+			'text' => 'Sorry, admins only!',
+		) );
+		return;
+	}
+
+	$channel = $matches[1];
+
+	$bot->send( array(
+		'type' => 'message',
+		'channel' => $message->channel,
+		'text' => "I'd join $channel, but you haven't taught me how yet.",
+	) );
+}, 10, 2 );
+
+/**
+ * Reload on request
+ */
+add_action( 'hm.slack.bot.message', function ( $message, $bot ) {
+	$pattern = '/^' . $bot->get_matchable_name() . ':? reload\b/i';
+	if ( ! preg_match( $pattern, $message->text ) ) {
+		return;
+	}
+	if ( ! $bot->is_admin( $message->user ) ) {
+		$bot->send( array(
+			'type' => 'message',
+			'channel' => $message->channel,
+			'text' => 'Sorry, admins only!',
+		) );
+		return;
+	}
+
+	$bot->send( array(
+		'type' => 'message',
+		'channel' => $message->channel,
+		'text' => 'Be back in a jiffy.',
+	) );
+	$bot->disconnect();
+}, 10, 2 );
 
 /**
  * Reply to hello!
