20610b0fcc651f36674d1f6b1f53a6171ba3889c
Ryan McCue <me@ryanmccue.info>
2015-03-06 14:05:12 +1000

    Ensure `wp` is run in the correct dir

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,4 @@ WP_CLI_BIN=${WP_CLI_BIN:-"$(which wp)"}
 ARGS="hm-slackbot run"
 PIDFILE="$HOME/.slackbot.pid"
 
-start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --exec $WP_CLI_BIN -- $ARGS
+start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --chdir . --exec $WP_CLI_BIN -- $ARGS
